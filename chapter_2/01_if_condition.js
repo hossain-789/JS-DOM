ef906a2c9b80274190b8e63cc0eab4b628a2d775
Hossain Ahmed <hossainahmed.7709@gmail.com>
2024-03-27 00:32:59 +0600

    chapter two

---
+++ chapter_2/01_if_condition.js
@@ -0,0 +1,25 @@
+//if condition
+/* Use the if statement to specify a block of JavaScript 
+code to be executed if a condition is true. */
+
+var a = 10
+var b = 20
+
+if (a > b) {
+    console.log('A is greather than B')
+}
+
+if (a < b) {
+    console.log('B is greather than A')
+}
+
+
+var n = 8
+
+if (n % 2 === 0) {
+    console.log(n + 'is Even Number')
+}
+
+if(n % 2 === 1) {
+    console.log(n + 'is Odd number')
+}
