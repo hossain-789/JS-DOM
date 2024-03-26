635fafa81123dcd437d7113918997e495246ad3f
Hossain Ahmed <hossainahmed.7709@gmail.com>
2024-03-26 14:44:20 +0600

    arethmatic operator

---
+++ chapter_1/10.operator.js
@@ -0,0 +1,36 @@
+// Arithmetic Operator
+// + - * / % ++ --
+
+// var a = 11
+// var b = 6
+
+// console.log(a % b);
+
+// Incremental
+// pre Incremental - post incremental
+
+// console.log(a++) // post Incremental
+// console.log(a);
+
+// console.log(--b);
+
+
+//assignment operator
+
+var a = 10
+var b = 20
+
+a += b
+console.log(a)
+
+a -= b
+console.log(a)
+
+a *= b 
+console.log(a)
+
+a /= b
+console.log(a)
+
+a %= b
+console.log(a)
