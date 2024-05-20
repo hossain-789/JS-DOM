aa2a81a04e00c33a3accb0b4707e89978c36e414
Hossain Ahmed <hossainahmed.7709@gmail.com>
2024-05-20 20:19:42 +0600

    last update

---
+++ chapter_3/3_doWhileLoop.js
@@ -0,0 +1,5 @@
+var isRunning = false
+
+do {
+    console.log('I am running');
+} while (isRunning)
---
+++ chapter_3/4_nestedLoops.js
@@ -0,0 +1,37 @@
+// var n = 5
+
+// for (var i = 1; i<=n; i++) {
+//     var result = ''
+//     for (var j = 1; j<=i; j++) {
+//         result += j + ' '
+//     }
+//     console.log(result);
+// }
+
+
+// var btn = 10
+// for (var i = 1; i<=btn; i++) {
+//     var result = ''
+//     for (var j = 1; j<=i; j++){
+//         result += j + ' '
+//     } 
+//     console.log(result);
+// }
+
+// var n = 4
+// for(var i = 1; i<=n; i++){
+//     var result = ''
+//     for (var j = 1; j<=i; j++){
+//         result += j + ' '
+//     }
+//     console.log(result);
+// }
+
+var n = 5
+for (var i = 1; i<=n; i++) {
+    var result = ''
+    for (var j = 1; j<=i; j++){
+        result += '* '
+    }
+    console.log(result);
+}
